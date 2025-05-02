0e16766e4532c7885d20efdcc038303080ce34d7
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-05-02 16:14:00 +0530

    created add new snippet server action

--- src/app/snippet/add/page.tsx
+++ src/app/snippet/add/page.tsx
@@ -2,18 +2,34 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const CreateSnippet = () => {
+  const createSnippet = async (formData: FormData) => {
+    "use server";
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    await prisma.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    redirect("/");
+  };
   return (
-    <form className="flex flex-col gap-y-4 w-1/4">
+    <form action={createSnippet} className="flex flex-col gap-y-4 w-1/4">
       <div className="flex flex-col gap-y-2">
         <Label>Title</Label>
-        <Input placeholder="Title" id="title" />
+        <Input name="title" placeholder="Title" id="title" />
       </div>
       <div className="flex flex-col gap-y-2">
         <Label>Code</Label>
-        <Textarea placeholder="Code" id="code" />
+        <Textarea name="code" placeholder="Code" id="code" />
       </div>
       <Button className="mt-4" type="submit">
         Save
